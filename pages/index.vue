5f7e4a5e4f2305c4277fc58341ca6756a292d786
ICT of Kalla Institute <ict@kallainstitute.ac.id>
2025-01-02 13:48:35 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="flex flex-col justify-center">
-    <HomeTwitter />
+    <!-- <HomeTwitter /> -->
     <HomeHero />
-    <HomeFeatures />
+    <!-- <HomeFeatures />
     <HomeLogos />
-    <HomeCta />
+    <HomeCta /> -->
   </main>
 </template>
