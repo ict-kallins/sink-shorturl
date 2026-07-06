57ca5435068afc6dc51cabf4398f333e94ad0da1
ICT of Kalla Institute <ict@kallainstitute.ac.id>
2026-07-06 14:42:49 +0800

    Change identity to Kalla Institute

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="flex flex-col justify-center">
     <HomeHero />
-    <HomeLogos />
-    <HomeFeatures />
-    <HomeStats />
-    <HomeTestimonials />
-    <HomeCta />
   </div>
 </template>
